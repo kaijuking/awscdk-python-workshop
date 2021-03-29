68190ad94d6dccb2440c3b41382e72807e1e4f1e
Michael Field <kaijuking28@gmail.com>
2021-03-29 15:25:52 -0700

    added hello lambda

--- awscdk_python_workshop/awscdk_python_workshop_stack.py
+++ awscdk_python_workshop/awscdk_python_workshop_stack.py
@@ -1,9 +1,6 @@
 from aws_cdk import (
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
-    core
+    core,
+    aws_lambda as _lambda
 )
 
 
@@ -12,13 +9,10 @@ class AwscdkPythonWorkshopStack(core.Stack):
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "AwscdkPythonWorkshopQueue",
-            visibility_timeout=core.Duration.seconds(300),
+        # Define an AWS Lambda resource
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime = _lambda.Runtime.PYTHON_3_8,
+            code = _lambda.Code.asset('lambda'),
+            handler = 'hello.handler'
         )
-
-        topic = sns.Topic(
-            self, "AwscdkPythonWorkshopTopic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
